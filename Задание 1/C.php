9bb80497e146e985ab4e590859666914b276eb96
Anatoly Samaris <anatolysamaris@gmail.com>
2024-02-16 01:35:07 +0300

    task C completed

--- Задание 1/C.php
+++ Задание 1/C.php
@@ -16,7 +16,72 @@ class Solution
     public function solve(string $file)
     {
         $input = file($file, FILE_IGNORE_NEW_LINES);
-        return;
+        $answer = array();
+
+        foreach ($input as $line) {
+            $request = explode('> ', $line);
+            $str = trim($request[0], '<');
+            $args = explode(' ', $request[1]);
+            $type = $args[0];
+
+            if ($type == 'S' or $type == 'N') {
+                $n = $args[1];
+                $m = $args[2];
+            }
+
+            print_r($request);
+            switch ($type) {
+                case 'S':
+                    $str_len = strlen($str);
+                    if ($n <= $str_len && $str_len <= $m) {
+                        $answer[] = 'OK';
+                    } else {
+                        $answer[] = 'FAIL';
+                    }
+                    break;
+                case 'N':
+                    if ($n <= $str && $str <= $m && preg_match('/^-?\d+$/', $str)) {
+                        $answer[] = 'OK';
+                    } else {
+                        $answer[] = 'FAIL';
+                    }
+                    break;
+                case 'P':
+                    $phone_pattern = '/^\+7 \(\d{3}\) \d{3}\-\d{2}\-\d{2}$/';
+                    if (preg_match($phone_pattern, $str)) {
+                        $answer[] = 'OK';
+                    } else {
+                        $answer[] = 'FAIL';
+                    }
+                    break;
+                case 'D':
+                    $date_format_1 = 'd.m.Y G:i';
+                    $date_format_2 = 'j.n.Y G:i';
+                    $date = DateTime::createFromFormat($date_format_1, $str);
+                    if (!$date) {
+                        $date = DateTime::createFromFormat($date_format_2, $str);
+                    }
+                    if ($date && 
+                        $date->format($date_format_1) == $str || 
+                        $date->format($date_format_2) == $str) {
+                        $answer[] = 'OK';
+                    } else {
+                        $answer[] = 'FAIL';
+                    }
+                    break;
+                case 'E':
+                    $email_pattern = '/^[a-zA-Z0-9_]{4,30}@[a-zA-Z]{2,30}\.[a-z]{2,10}$/';
+                    if (preg_match($email_pattern, $str) && $str[0] != '_') {
+                        $answer[] = 'OK';
+                    } else {
+                        $answer[] = 'FAIL';
+                    }
+                    break;
+            }
+            echo $answer[count($answer)-1]."\n\n";
+        }
+
+        return $answer;
     }
 }
 
@@ -26,5 +91,5 @@ $tester = new Tester($solution);
 
 $tester->test(
     tests_dir: $solution->TEST_PATH,
-    //one_test: 1
+    //one_test: 4
 );
